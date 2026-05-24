115ae3acbd50358094087353dae2c76ea6ab2d11
fi-res <nta16022013@gmail.com>
2026-05-24 14:07:02 +0300

    fix(clan): TopClans.refresh clears value

--- itd/clan.py
+++ itd/clan.py
@@ -14,5 +14,5 @@ class TopClans(ITDBaseModel, list[Clan]):
         self.refresh()
 
     def refresh(self):
-        # TODO!!
+        self.clear()
         self.extend([Clan.model_validate(clan) for clan in get_top_clans(self.client).json()['clans']])
